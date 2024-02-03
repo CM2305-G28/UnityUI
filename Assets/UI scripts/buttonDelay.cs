0abb424c473b095b139cc45d5c06cdfd265118f3
Aidan Davey <DaveyA5@cardiff.ac.uk>
2024-02-03 20:39:54 +0000

    Added delays to the login buttons so they aren't glitchy anymore.

--- Assets/UI scripts/buttonDelay.cs
+++ Assets/UI scripts/buttonDelay.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class buttonDelay : MonoBehaviour
+public class ButtonDelay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //This script just tidies up the login animation so buttons dont show up before they animate themselves in
+    public Button Button1;
+public Button Button2;
+public Button Button3;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+public void Button1Clicked()
+{
+    //button is clicked
+    StartCoroutine(delayButton());
+}
+
+IEnumerator delayButton()
+{
+    Debug.Log(Time.time);
+    yield return new WaitForSeconds(0.8f);
+    Debug.Log(Time.time);
+
+    // Buttons are set interactable after 0.8 second
+    Button2.interactable=true;
+    Button3.interactable=true;
+}
+
+    
 }
