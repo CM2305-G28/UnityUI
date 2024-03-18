5d7a1858d80c8f6e3ef7c8a833b525e4455adab5
Aidan Davey <DaveyA5@cardiff.ac.uk>
2024-03-18 10:24:08 +0000

    Objectives have been added, rn you can just toggle them on and off and see how the porgress circle updates, will change its creation to be a dropdown rather than inputfield in the futute

--- Assets/UI scripts/HomeCircle.cs
+++ Assets/UI scripts/HomeCircle.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using System;
+using System.Linq;
 
 public class HomeCircle : MonoBehaviour
 {
@@ -23,6 +24,27 @@ public class HomeCircle : MonoBehaviour
 
     public void DecreaseFill(){
         uiCircle.fillAmount-=steps;
+        uiText.text=Math.Round(uiCircle.fillAmount*100).ToString()+"%";
+    }
+
+    public void updateFill(GameObject taskList){
+        List<Toggle> toggles = taskList.GetComponentsInChildren<Toggle>().ToList();
+        float on=0;
+        foreach (Toggle t in toggles){
+            if (t.isOn){
+                on++;
+                
+            }
+        }
+        float percent = on/toggles.Count;
+        uiCircle.fillAmount=percent;
+        uiText.text=Math.Round(uiCircle.fillAmount*100).ToString()+"%";
+    }
+
+    public void onTaskDelete(GameObject taskList){
+        List<Toggle> toggles = taskList.GetComponentsInChildren<Toggle>().ToList();
+        float percent = 1/toggles.Count;
+        uiCircle.fillAmount-=percent;
         uiText.text=Math.Round(uiCircle.fillAmount*100).ToString()+"%";
     }
 
