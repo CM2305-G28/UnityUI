fa296979e319bf1dee8a1695b8ff42c9db6bea8b
Aidan Davey <DaveyA5@cardiff.ac.uk>
2024-04-26 22:13:12 +0100

    good luck fixing patryk

--- Assets/Cipher scripts 1/Login.cs
+++ Assets/Cipher scripts 1/Login.cs
@@ -13,15 +13,15 @@ public class Login : MonoBehaviour
     public TMP_InputField username;
     public TMP_InputField password;
 
-    public void Login(){
-        Debug.Log("Connecting to server...")
+    public void login(){
+        Debug.Log("Connecting to server...");
         Client client = new Client();
-        Debug.Log("Connected, logging in...")
-        if (client.Login(username.text, password password.text)){
+        Debug.Log("Connected, logging in...");
+        if (client.Login(username.text, password.text)){
             Debug.Log("Logged in successfully!")
         }
         else{
-            Debug.Log("unable to login...")
+            Debug.Log("unable to login...");
         }
     }
 }
