892b8444108213673f0384d0d382864553b4c23e
0x6AB3 <patrykgluszkiewicz@gmail.com>
2024-05-01 21:57:33 +0100

    Split slides into scripts

---
+++ Assets/Cipher scripts 1/SSL_slide_2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class SSL_slide_2  : MonoBehaviour
+{
+    public TMP_InputField clientCryptoInformation;
+    public TextMeshProUGUI clientStatus;
+    // 2
+    public void ServerHello()
+    {
+        clientCryptoInformation.text = "USING " +  GenerateCryptoInfo.GenerateCryptographicInformation().Split("\n")[0];
+        clientStatus.text = "Verifying.";
+        Invoke("Wait", 1f); // 1 second wait
+        clientStatus.text = "Verifying..";
+        Invoke("Wait", 1f);
+        clientStatus.text = "Verifying...";
+        Invoke("Wait", 1f);
+        clientStatus.text = "Verified!";
+        Invoke("Wait", 1f);
+    }
+
+    private void Wait()
+    {
+        Debug.Log("waiting...");
+    }
+}
