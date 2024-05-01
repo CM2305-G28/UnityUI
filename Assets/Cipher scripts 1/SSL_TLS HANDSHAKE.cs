6d765ba56967fb662b67ebf93fb0956e1a25a04c
0x6AB3 <patrykgluszkiewicz@gmail.com>
2024-05-01 20:59:10 +0100

    Add SSL_TLS HANDSHAKE script

---
+++ Assets/Cipher scripts 1/SSL_TLS HANDSHAKE.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using TMPro;
+
+public class SSL_TLS_HANDSHAKE : MonoBehaviour
+{
+    public TMP_InputField clientMessage;
+    public TMP_InputField serverStatus;
+    public TMP_InputField clientStatus;
+    public TMP_InputField clientCryptoInformation;
+    
+    private void GenerateCryptographicInformation()
+    {
+        var cryptoInfo = "SUPPORTED CIPHER SUITE: TLS_AES_256_GCM_SHA384\n";
+        cryptoInfo += "VERSION: TLS 1.3\n";
+        cryptoInfo += $"CLIENT-GENERATED NUMBER: {Random.Range(1000000000, 9000000000)}";
+        clientCryptoInformation.text = cryptoInfo;
+    }
+    
+    // 1
+    public void ClientHello()
+    {
+        serverStatus.text = $"SERVER RECEIVED: {clientMessage.text}";
+        clientStatus.text = $"MESSAGE SENT!";
+        GenerateCryptographicInformation();
+    }
+}
