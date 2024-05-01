19d72d31cfc2f4a642d9b10ecf4240e022683051
0x6AB3 <patrykgluszkiewicz@gmail.com>
2024-05-01 21:48:22 +0100

    Fix

--- Assets/Cipher scripts 1/SSL_TLS HANDSHAKE.cs
+++ Assets/Cipher scripts 1/SSL_TLS HANDSHAKE.cs
@@ -20,10 +20,6 @@ public class SSL_TLS_HANDSHAKE : MonoBehaviour
     public void ClientHello()
     {
         serverStatus.text = $"SERVER RECEIVED: {clientMessage.text}";
-<<<<<<< Updated upstream
-        clientStatus.text = $"MESSAGE SENT!";
-        clientCryptoInformation.text = GenerateCryptographicInformation();
-=======
         clientStatus.text = "MESSAGE SENT!";
         clientCryptoInformation.text = GenerateCryptographicInformation();
     }
@@ -45,26 +41,5 @@ public class SSL_TLS_HANDSHAKE : MonoBehaviour
     private void Wait()
     {
         Debug.Log("waiting...");
->>>>>>> Stashed changes
     }
-    
-    // 2
-    public void ServerHello()
-    {
-        clientCryptoInformation.text = "USING " +  GenerateCryptographicInformation().Split("\n")[0];
-        clientStatus.text = "Verifying.";
-        Invoke("Wait", 1f); // 1 second wait
-        clientStatus.text = "Verifying..";
-        Invoke("Wait", 1f);
-        clientStatus.text = "Verifying...";
-        Invoke("Wait", 1f);
-        clientStatus.text = "Verified!";
-        Invoke("Wait", 1f);
-    }
-    
-    private void Wait()
-    {
-        Debug.Log("waiting...");
-    }
-
 }
