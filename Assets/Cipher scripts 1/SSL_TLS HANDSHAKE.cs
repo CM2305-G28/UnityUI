f468bcf335c557a86d90219d3c62669a1f4d7fa8
0x6AB3 <patrykgluszkiewicz@gmail.com>
2024-05-01 21:46:04 +0100

    Implement SSL_TLS HANDSHAKE slide 2

--- Assets/Cipher scripts 1/SSL_TLS HANDSHAKE.cs
+++ Assets/Cipher scripts 1/SSL_TLS HANDSHAKE.cs
@@ -3,24 +3,68 @@ using TMPro;
 
 public class SSL_TLS_HANDSHAKE : MonoBehaviour
 {
+    // 1
     public TMP_InputField clientMessage;
     public TextMeshProUGUI serverStatus;
     public TextMeshProUGUI clientStatus;
     public TMP_InputField clientCryptoInformation;
     
-    private void GenerateCryptographicInformation()
+    private string GenerateCryptographicInformation()
     {
-        var cryptoInfo = "SUPPORTED CIPHER SUITE: TLS_AES_256_GCM_SHA384\n";
+        string cryptoInfo = "SUPPORTED CIPHER SUITE: TLS_AES_256_GCM_SHA384\n";
         cryptoInfo += "VERSION: TLS 1.3\n";
         cryptoInfo += $"CLIENT-GENERATED NUMBER: {Random.Range(1000000000, 9000000000)}";
-        clientCryptoInformation.text = cryptoInfo;
+        return cryptoInfo;
     }
     
-    // 1
     public void ClientHello()
     {
         serverStatus.text = $"SERVER RECEIVED: {clientMessage.text}";
+<<<<<<< Updated upstream
         clientStatus.text = $"MESSAGE SENT!";
-        GenerateCryptographicInformation();
+        clientCryptoInformation.text = GenerateCryptographicInformation();
+=======
+        clientStatus.text = "MESSAGE SENT!";
+        clientCryptoInformation.text = GenerateCryptographicInformation();
     }
+    
+    // 2
+    public void ServerHello()
+    {
+        clientCryptoInformation.text = "USING " +  GenerateCryptographicInformation().Split("\n")[0];
+        clientStatus.text = "Verifying.";
+        Invoke("Wait", 1f); // 1 second wait
+        clientStatus.text = "Verifying..";
+        Invoke("Wait", 1f);
+        clientStatus.text = "Verifying...";
+        Invoke("Wait", 1f);
+        clientStatus.text = "Verified!";
+        Invoke("Wait", 1f);
+    }
+    
+    private void Wait()
+    {
+        Debug.Log("waiting...");
+>>>>>>> Stashed changes
+    }
+    
+    // 2
+    public void ServerHello()
+    {
+        clientCryptoInformation.text = "USING " +  GenerateCryptographicInformation().Split("\n")[0];
+        clientStatus.text = "Verifying.";
+        Invoke("Wait", 1f); // 1 second wait
+        clientStatus.text = "Verifying..";
+        Invoke("Wait", 1f);
+        clientStatus.text = "Verifying...";
+        Invoke("Wait", 1f);
+        clientStatus.text = "Verified!";
+        Invoke("Wait", 1f);
+    }
+    
+    private void Wait()
+    {
+        Debug.Log("waiting...");
+    }
+
 }
