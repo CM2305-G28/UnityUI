e1bc0c0e23edcbe0ab6ad3cedaf19fb9e3929c3f
0x6AB3 <patrykgluszkiewicz@gmail.com>
2024-05-01 22:16:55 +0100

    Implement slide 3

--- Assets/Cipher scripts 1/SSL_slide_2.cs
+++ Assets/Cipher scripts 1/SSL_slide_2.cs
@@ -5,6 +5,7 @@ public class SSL_slide_2  : MonoBehaviour
 {
     public TMP_InputField clientCryptoInformation;
     public TextMeshProUGUI clientStatus;
+    public TextMeshProUGUI serverStatus;
     // 2
     public void ServerHello()
     {
@@ -13,6 +14,7 @@ public class SSL_slide_2  : MonoBehaviour
             "\nCertificate:\t0569a72a23ea2234562515d477c93056963cd3ccffb7f43e5cfbca61f259748c";
         clientCryptoInformation.text +=
             "\nPublic Key:\tf1cd7669b2e495a46eed6927a4377e7c82140803c600bd3ade7972d8bade49ea";
-        clientStatus.text = "Verifying... [Select 'Next']";
+        clientStatus.text = "VERIFYING... [Select 'Next']";
+        serverStatus.text = "MESSAGE SENT!";
     }
 }
