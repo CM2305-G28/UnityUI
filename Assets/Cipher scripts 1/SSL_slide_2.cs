ea34e7063c9df3c1010387298c6843a3fc11c8e4
0x6AB3 <patrykgluszkiewicz@gmail.com>
2024-05-01 22:06:50 +0100

    Implement SSL slide 2 output change

--- Assets/Cipher scripts 1/SSL_slide_2.cs
+++ Assets/Cipher scripts 1/SSL_slide_2.cs
@@ -8,19 +8,11 @@ public class SSL_slide_2  : MonoBehaviour
     // 2
     public void ServerHello()
     {
-        clientCryptoInformation.text = "USING " +  GenerateCryptoInfo.GenerateCryptographicInformation().Split("\n")[0];
-        clientStatus.text = "Verifying.";
-        Invoke("Wait", 1f); // 1 second wait
-        clientStatus.text = "Verifying..";
-        Invoke("Wait", 1f);
-        clientStatus.text = "Verifying...";
-        Invoke("Wait", 1f);
-        clientStatus.text = "Verified!";
-        Invoke("Wait", 1f);
-    }
-
-    private void Wait()
-    {
-        Debug.Log("waiting...");
+        clientCryptoInformation.text = "USING " + GenerateCryptoInfo.GenerateCryptographicInformation().Split("\n")[0];
+        clientCryptoInformation.text +=
+            "\nCertificate:\t0569a72a23ea2234562515d477c93056963cd3ccffb7f43e5cfbca61f259748c";
+        clientCryptoInformation.text +=
+            "\nPublic Key:\tf1cd7669b2e495a46eed6927a4377e7c82140803c600bd3ade7972d8bade49ea";
+        clientStatus.text = "Verifying... [Select 'Next']";
     }
 }
