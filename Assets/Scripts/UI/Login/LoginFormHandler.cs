7105b167f8c10335d550877d8046d62f3a8142c0
Will <16578982+CloudG360@users.noreply.github.com>
2024-05-02 19:00:46 +0100

    Fixes to AnimClient now result in a platform error - cool

---
+++ Assets/Scripts/UI/Login/LoginFormHandler.cs
@@ -0,0 +1,34 @@
+﻿using SecureAnim;
+using SecureAnimClient;
+using TMPro;
+using UnityEngine;
+using Util;
+
+namespace UI.Login
+{
+    public class LoginFormHandler : MonoBehaviour
+    {
+        
+        static LoginFormHandler() {
+            BackendSupport.InitLogging();
+        }
+        
+        public LoginType lastSelectedState = LoginType.STUDENT;
+    
+        public TMP_InputField username;
+        public TMP_InputField password;
+
+        public void Login() {
+            Debug.Log("Connecting to server...");
+
+            ProtocolHelp.LogInfo("ProtocolHelp Logger is working");
+            
+            Client client = new Client();
+            Debug.Log("Connected, logging in...");
+        
+            if (client.Login(username.text, password.text))
+                Debug.Log("Logged in successfully!");
+            else Debug.Log("unable to login...");
+        }
+    }
+}
